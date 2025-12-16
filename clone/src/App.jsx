51a6f69116090abc0fe41fb5ffaa6f9223f3c0ee
Srisabarish Velusamy <srisabrishv@parkcorporates.com>
2025-12-16 19:51:53 +0530

    new change

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+
 import Navbar from './Components/Navbar/Navbar'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Video from './Pages/Videos'
+
+
+
 
 const App = () => {
   return (
     <div>
-
       <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video:categoryId/:videoId' element={<Video/>} />
+      </Routes>
+
     </div>
   )
 }
